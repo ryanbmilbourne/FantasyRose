11e73a10b003418e664315cb36b538c7c7d26e9b
Ryan Milbourne <Ryan.Milbourne@viasat.com>
2015-01-09 13:35:41 -0800

    non-admins could access the admin page

--- index.js
+++ index.js
@@ -137,7 +137,11 @@ app.get('/rules', function(req, res){
 
 //basic middleware for admin related pages to check for authentication first.
 app.use('/admin', function(req, res, next){
-    if(req.isAuthenticated()){ return next();}
+    if(req.isAuthenticated()){
+        if(req.user.admin){ return next();}
+        req.session.error = 'You must be signed in as an Adminstrator to view the requested page.';
+        res.redirect('/');
+    }
     req.session.error = 'You must be signed in to view the requested page.';
     res.redirect('/signin');
 });
